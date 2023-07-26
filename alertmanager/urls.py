024b0ba3951813c539942ab390acbbd1a1e508ff
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-26 20:20:53 +0100

    Initial Setup for Django Project

--- alertmanager/urls.py
+++ alertmanager/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for AlertManager project.
+URL configuration for alertmanager project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
