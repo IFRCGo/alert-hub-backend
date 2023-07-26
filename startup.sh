01e6002edddb5f6ea577a092cc82b11e477850b6
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-27 00:39:34 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
-daphne -b 0.0.0.0 -p 8000 capaggregator.asgi:application & celery -A capaggregator worker -l info --pool=solo
+gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
+    '-' --error-logfile '-' --bind=0.0.0.0:8000 \
+     --chdir=/home/site/wwwroot alertmanager.wsgi #& celery -A alertmanager worker -l info --pool=solo
