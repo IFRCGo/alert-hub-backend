27921d4489cb6696277a25bba214ecec197b0d15
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-08-01 12:49:16 +0100

    fixing bugs on azure

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
+gunicorn --workers 2 --threads 4 --timeout 5000 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000 \
      --chdir=/home/site/wwwroot alertmanager.wsgi & celery -A alertmanager worker -l info --pool=solo
