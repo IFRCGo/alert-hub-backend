f6b5dc5be50461db92e6d91f14724000612063a8
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-08-03 12:40:01 +0100

    Fix bugs on deleting regions

--- cache/region_cache.py
+++ cache/region_cache.py
@@ -37,6 +37,15 @@ def initialise_region_cache():
     cache.set("regions", regions_dict, timeout=None)
     cache.set("regions_in_json", json.dumps(regions_dict,indent=None), timeout=None)
 
+
+def cache_imcoming_alerts_info_into_region_cache(alert_id):
+    alert = CapFeedAlert.objects.filter(id=alert_id).first()
+    regions_dict = cache.get("regions")
+    if alert != None:
+        append_alert_info_to_region_cache(regions_dict,alert)
+        cache.set("regions", regions_dict, timeout=None)
+        cache.set("regions_in_json", json.dumps(regions_dict, indent=None), timeout=None)
+
 def append_alert_info_to_region_cache(regions_dict, alert):
     alert_region_id = alert.country.region.id
     alert_country_id = alert.country.id
@@ -65,49 +74,45 @@ def append_alert_info_to_region_cache(regions_dict, alert):
         else:
             certainty_dict[certainty] = certainty_dict[certainty] + 1
 
-def cache_imcoming_alerts_info_into_region_cache(alert_id):
-    alert = CapFeedAlert.objects.filter(id=alert_id).first()
-    regions_dict = cache.get("regions")
-    if alert != None:
-        append_alert_info_to_region_cache(regions_dict,alert)
-        cache.set("regions", regions_dict, timeout=None)
-        cache.set("regions_in_json", json.dumps(regions_dict, indent=None), timeout=None)
-
-
 def remove_alerts_info_from_regions_cache(alert_id):
-    alert_dict = cache.get("alerts")
+    cache_keys = cache.client.keys('*')
+    alert_cache_key = "alert" + str(alert_id)
+
+    if alert_cache_key not in cache_keys:
+        return f"Alert: {alert_id} is not in the cache."
+    alert_dict = json.loads(cache.get(alert_cache_key))
     regions_dict = cache.get("regions")
-    if alert_id in alert_dict:
-        alert_region_id = alert_dict["region_id"]
-        alert_country_id = alert_dict["country_id"]
-        alert_info = alert_dict["info"]
-        if len(alert_info) == 0:
-            return None
-        severity = alert_info[0]["severity"]
-        urgency = alert_info[0]["urgency"]
-        certainty = alert_info[0]["certainty"]
 
-        country_dict = regions_dict[alert_region_id]["countries"][alert_country_id]
-        severity_dict = country_dict["severity"]
-        urgency_dict = country_dict["urgency"]
-        certainty_dict = country_dict["certainty"]
-        if severity in severity_dict:
-            severity_dict[severity] -= 1
-            if severity_dict[severity] == 0:
-                del severity_dict[severity]
-        if urgency in urgency_dict:
-            urgency_dict[urgency] -= 1
-            if urgency_dict[urgency] == 0:
-                del urgency_dict[urgency]
+    alert_region_id = alert_dict["region_id"]
+    alert_country_id = alert_dict["country_id"]
+    alert_info = alert_dict["info"]
+    if len(alert_info) == 0:
+        return None
+    severity = alert_info[0]["severity"]
+    urgency = alert_info[0]["urgency"]
+    certainty = alert_info[0]["certainty"]
 
-        if certainty in certainty_dict:
-            certainty_dict[certainty] -= 1
-            if certainty_dict[certainty] == 0:
-                del certainty_dict[certainty]
-        cache.set("regions", regions_dict, timeout=None)
-        cache.set("regions_in_json", json.dumps(regions_dict, indent=None), timeout=None)
-        return f"Alert: {alert_id} is removed from regional level records."
-    return f"Alert: {alert_id} is not in the cache."
+    country_dict = regions_dict[alert_region_id]["countries"][alert_country_id]
+    severity_dict = country_dict["severity"]
+    urgency_dict = country_dict["urgency"]
+    certainty_dict = country_dict["certainty"]
+    if severity in severity_dict:
+        severity_dict[severity] -= 1
+        if severity_dict[severity] == 0:
+            del severity_dict[severity]
+    if urgency in urgency_dict:
+        urgency_dict[urgency] -= 1
+        if urgency_dict[urgency] == 0:
+            del urgency_dict[urgency]
+
+    if certainty in certainty_dict:
+        certainty_dict[certainty] -= 1
+        if certainty_dict[certainty] == 0:
+            del certainty_dict[certainty]
+    cache.set("regions", regions_dict, timeout=None)
+    cache.set("regions_in_json", json.dumps(regions_dict, indent=None), timeout=None)
+    return f"Alert: {alert_id} is removed from regional level records."
+
 
 def get_regions():
     return cache.get("regions_in_json")
