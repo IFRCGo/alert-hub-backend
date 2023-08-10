defaab0d9748cba6d4b29ad01c6a4d2d934676d2
Alfiejz <alfiejz@outlook.com>
2023-08-10 21:00:45 +0100

    Fixed districts naming bug

--- cache/admin1s_cache.py
+++ cache/admin1s_cache.py
@@ -4,18 +4,18 @@ from .models import CapFeedFeed, CapFeedCountry
 
 
 def initialise_admin1s_cache():
-    districts_data = {'countries': []}
+    admin1s_data = {'countries': []}
     country_ids = set(CapFeedFeed.objects.all().values_list('country', flat=True))
     for country_id in country_ids:
         country = CapFeedCountry.objects.get(id=country_id)
-        country_data = {'id': country.id, 'name': country.name, 'districts': []}
+        country_data = {'id': country.id, 'name': country.name, 'admin1s': []}
         for district in country.capfeedadmin1_set.all():
             district_data = {'id': district.id, 'name': district.name}
-            country_data['districts'].append(district_data)
-        districts_data['countries'].append(country_data)
+            country_data['admin1s'].append(district_data)
+        admin1s_data['countries'].append(country_data)
 
-    cache.set("districts", districts_data, timeout = None)
+    cache.set("admin1s", admin1s_data, timeout = None)
 
 def get_admin1s():
-    districts_cache_key = "districts"
-    return cache.get(districts_cache_key, {})
+    admin1s_cache_key = "admin1s"
+    return cache.get(admin1s_cache_key, {})
