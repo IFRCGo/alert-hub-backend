67a73689fbb371c6fe40452a9d51b1979261fcc3
thenav56 <ayernavin@gmail.com>
2024-08-28 14:32:20 +0545

    Replace deprecated docker-compose command

--- main/tests/test_fake.py
+++ main/tests/test_fake.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 class FakeTest(TestCase):
     """
     This test is for running migrations only
-    docker-compose run --rm server ./manage.py test -v 2 --pattern="deep/tests/test_fake.py"
+    docker compose run --rm server ./manage.py test -v 2 --pattern="deep/tests/test_fake.py"
     """
 
     def test_fake(self):
